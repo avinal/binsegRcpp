2939eeae84fb54713c45115316acc43c4f20758c
avinal <185067@nith.ac.in>
2021-01-11 11:43:34 +0530

    corrected header defination

--- src/binseg_normal.h
+++ src/binseg_normal.h
@@ -6,6 +6,7 @@ int binseg_normal
 (const double *data_vec, const int n_data, const int max_segments,
  int *seg_end, double *cost,
  double *before_mean, double *after_mean,
+ double *before_var, double *after_var,
  int *, int *,
  int *invalidates_index, int *invalidates_before);
 
